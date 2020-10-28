8bc2435365f39fe7a602ad570f38cd187d2f1fe5
Jean Atsumi Flaherty <kobejean@me.com>
2020-10-28 13:14:52 -0600

    added routing

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,19 @@
 <script>
-	export let name;
+	import { Router, Route, Link } from "svelte-routing";
+	import Home from "./routes/Home.svelte";
+	import About from "./routes/About.svelte";
+	export let url = ""; //This property is necessary declare to avoid ignore the Router
 </script>
-
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<Router url="{url}">
+	<nav>
+	   <Link to="/">Home</Link>
+	   <Link to="about">About</Link>
+	</nav>
+	<div>
+		<Route path="about" component="{About}" />
+		<Route path="/"><Home /></Route>
+	</div>
+</Router>
 
 <style>
 	main {
