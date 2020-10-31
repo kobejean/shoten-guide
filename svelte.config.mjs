01a4d5017de1858fae1e9bba7e5f8961b91201eb
Jean Atsumi Flaherty <kobejean@me.com>
2020-10-31 17:16:27 -0600

    SCSSの設置

---
+++ svelte.config.mjs
@@ -0,0 +1,7 @@
+import sveltePreprocess from 'svelte-preprocess'
+// this file is needed for svelte for vs code extension to work 
+const preprocess = sveltePreprocess()
+
+export default {
+    preprocess,
+};
