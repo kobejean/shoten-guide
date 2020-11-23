e85260b0225c772797d52da122ae05c2f6a235b1
Jean Atsumi Flaherty <kobejean@me.com>
2020-11-22 17:29:43 -0700

    chache x

--- src/client.js
+++ src/client.js
@@ -3,6 +3,7 @@ import { getInitialLocale } from './services/i18n/initialization.js'
 import { FALLBACK_LOCAL } from './services/i18n/constants.js'
 import { init } from 'svelte-i18n'
 
+console.log('client js')
 // initialize svelte-i18n as soon as possible to prevent flickering
 init({
   fallbackLocale: FALLBACK_LOCAL,
