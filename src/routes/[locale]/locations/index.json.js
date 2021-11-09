c1146c6e405a4e3e5996bf1de049660b7f7a6964
Jean Atsumi Flaherty <kobejean@me.com>
2021-11-09 21:58:15 +0900

    Revert some changes

--- src/routes/[locale]/locations/index.json.js
+++ src/routes/[locale]/locations/index.json.js
@@ -8,13 +8,13 @@ export function get(req, res) {
 
   if (data) {
     res.writeHead(200, {
-      'Content-Type': 'application/json;charset=utf-8',
+      'Content-Type': 'application/json',
     })
 
     res.end(JSON.stringify({ data }))
   } else {
     res.writeHead(404, {
-      'Content-Type': 'application/json;charset=utf-8',
+      'Content-Type': 'application/json',
     })
 
     res.end(
